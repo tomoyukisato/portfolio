fbc1640c1694800e5b2ed374625c02c6a5680a0e
Tomoyuki Sato <t.sato@freedive.co.jp>
2019-10-13 16:49:26 +0900

    first upload img/js/css

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// $(function() {
+//     // let images = document.getElementsByName('images');
+//     //マウスを乗せたら発動
+//     $("").hover(
+//         function(){
+//                         $(this).stop().animate({'marginTop':'-180px'},'normal');
+//                     },
+//                     function () {
+//                         $(this).stop().animate({'marginTop':'-0px'},'normal');
+//                     }
+            
+//     );
+//   });
